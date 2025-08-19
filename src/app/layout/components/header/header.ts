a98563b1884c80cb4304dfab310798683e6eeb3a
alsaeedfayed <alsaeedfayed@gmail.com>
2025-08-19 15:04:01 -0400

    feat: :fire: header => prepare the design for input search

--- src/app/layout/components/header/header.ts
+++ src/app/layout/components/header/header.ts
@@ -1,11 +1,31 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  Output,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   @Output() menuClicked = new EventEmitter<void>();
+  searchTerm: WritableSignal<string> = signal('');
+  searchResults: any[] = [];
+  onSearch(): void {
+    this.searchResults = ['Result 1', 'Result 2', 'Result 3'];
+    // if (this.searchTerm().trim()) {
+    //   this.searchResults = ['Result 1', 'Result 2', 'Result 3'].filter((r) =>
+    //     r.toLowerCase().includes(this.searchTerm().toLowerCase())
+    //   );
+    // } else {
+    //   this.searchResults = [];
+    // }
+  }
 }

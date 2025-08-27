c9f6d222ced7c0bb5ddf4387f3005382c5599398
alsaeedfayed <alsaeedfayed@gmail.com>
2025-08-27 17:02:30 -0400

    fix: :bug: fix context menu toggler and detect listner on the DOM

--- src/app/components/context-menu/context-menu.ts
+++ src/app/components/context-menu/context-menu.ts
@@ -1,8 +1,12 @@
 import {
+  AfterViewInit,
   Component,
+  ElementRef,
   EventEmitter,
+  inject,
   Input,
   Output,
+  Renderer2,
   signal,
   WritableSignal,
 } from '@angular/core';
@@ -13,10 +17,26 @@ import {
   templateUrl: './context-menu.html',
   styleUrl: './context-menu.scss',
 })
-export class ContextMenu {
+export class ContextMenu implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.clickListener = this.rendere.listen(
+      'document',
+      'click',
+      (event: MouseEvent) => {
+        const clickedInside = this.elRef.nativeElement.contains(event.target);
+        if (!clickedInside && this.isOpen()) {
+          this.isOpen.set(false);
+        }
+      }
+    );
+  }
   isOpen = signal(false);
   @Input() useDefault: boolean = false;
   @Output() menuAction = new EventEmitter<string>();
+  elRef = inject(ElementRef);
+  rendere = inject(Renderer2);
+  private clickListener!: () => void;
+
   // actions = [
   //   { label: 'Play', value: 'play' },
   //   { label: 'Add to Playlist', value: 'add_to_playlist' },
@@ -26,10 +46,22 @@ export class ContextMenu {
   // ];
   toggleMenu() {
     this.isOpen.update((open) => !open);
+    setTimeout(() => this.setupInternalClickHandlers(), 0);
   }
 
   actionClicked(action: string) {
     this.menuAction.emit(action);
     this.isOpen.set(false); // auto-close
+  }
+
+  private setupInternalClickHandlers() {
+    const clickableElements = this.elRef.nativeElement.querySelectorAll(
+      '.context-menu__card li, .context-menu__card button, .context-menu__card [data-close-context]'
+    );
+    clickableElements.forEach((el: HTMLElement) => {
+      el.addEventListener('click', () => {
+        this.isOpen.set(false);
+      });
+    });
   }
 }

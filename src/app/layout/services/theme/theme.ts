6a347389a83280401a7339cd40d648e6642aaa59
alsaeedfayed <alsaeedfayed@gmail.com>
2025-09-01 12:36:45 -0400

    feat: :fire: Apply reactive storage on theme & when change theme in some tab it reflects in other tabs

--- src/app/layout/services/theme/theme.ts
+++ src/app/layout/services/theme/theme.ts
@@ -1,21 +1,30 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Inject, Injectable, PLATFORM_ID, signal } from '@angular/core';
+import { inject, Inject, Injectable, PLATFORM_ID, signal } from '@angular/core';
+import { ReactiveStorage } from '@app/core/services/storage/reactive-storage';
+export const THEME_KEY = 'app-theme';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Theme {
   private theme = signal<'light' | 'dark'>('light');
+  private storage = inject(ReactiveStorage);
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-    if (isPlatformBrowser(this.platformId)) {
-      const savedTheme = localStorage.getItem('theme') as
-        | 'light'
-        | 'dark'
-        | null;
-      if (savedTheme) {
-        this.theme.set(savedTheme);
-      }
+    const savedTheme = this.storage.get(THEME_KEY, 'light');
+    if (savedTheme) {
+      this.theme.set(savedTheme);
     }
+
+    // Watch changes from other tabs/windows
+    this.storage
+      .watch<'light' | 'dark'>(THEME_KEY, savedTheme)
+      .subscribe((value) => {
+        if (value !== this.theme()) {
+          this.theme.set(value);
+          this.setTheme(value);
+        }
+      });
+
     this.setTheme(this.theme());
   }
   getTheme() {
@@ -25,7 +34,7 @@ export class Theme {
     if (isPlatformBrowser(this.platformId)) {
       document.body.classList.remove('theme-dark', 'theme-light');
       document.body.classList.add(`theme-${theme}`);
-      localStorage.setItem('theme', theme); // persist
+      this.storage.set(THEME_KEY, theme); // persist
     }
   }
 
